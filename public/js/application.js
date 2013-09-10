1ffb22a23602a254148edbea23545efa530b5e65
dbcapprentice <dbcapprentice@gmail.com>
2013-09-09 19:30:00 -0700

    javascript - bind to form submission, add toggleSpinner function, trigger ajax call, add ajaxStart and ajaxStop functions

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,28 @@
-$(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
+function toggleSpinner(){
+  $('#spinner').toggle();
+}
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+$( document ).ajaxStart(function() {
+  toggleSpinner();
+});
+
+$( document ).ajaxStop(function() {
+  toggleSpinner();
+});
+
+
+$(document).ready(function() {
+  $('#get_tweets').on('submit', function(e){
+    e.preventDefault();
+
+    var params = $(this).serialize();
+    $.ajax({
+      type: "GET",
+      url:"/",
+      data: params
+    }).done(function(server_response){
+      $('#recent_tweets').html(server_response);
+    }).fail(function(){console.log('wah wah');
+  });
+});
 });
