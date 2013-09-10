c13b6c282b08174bdf69ed9fa33a6c17de385910
dbcapprentice <dbcapprentice@gmail.com>
2013-09-09 21:31:08 -0700

    POST route for new tweet

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,7 +2,6 @@ get '/' do
   params[:username] ? redirect("/#{params[:username]}") : erb(:index)
 end
 
-
 get '/:username' do
   unless TwitterUser.find_by_username(params[:username])
     TwitterUser.create(username: params[:username])
@@ -19,4 +18,17 @@ get '/:username' do
 
 end
 
+post '/tweets' do
+  #hardcode per instructions
+  # @user = TwitterUser.create(username: "llorgenimhan")
+  # @user.tweets.create(text: params[:user_tweet], tweet_time: Time.now)
 
+  # The above are only needed if we wnat to write to database here.
+  # Instead, we can make the choice that database writing will only
+  # happen via the GET methods above.
+
+  # The below sends the tweet out for the user configured in Environment
+  # via OAuth Token and OAuth secret (which are hardcoded).
+  Twitter.update(params[:user_tweet])
+  redirect '/' unless request.xhr?
+end
