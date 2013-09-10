17dd72a0b64c5c16eff81e6f92338ec5fa9f135c
dbcapprentice <dbcapprentice@gmail.com>
2013-09-09 21:32:51 -0700

    implement ajax for sending tweet. toggle spinner. disable form. append dom messages

--- public/js/application.js
+++ public/js/application.js
@@ -1,28 +1,54 @@
-function toggleSpinner(){
-  $('#spinner').toggle();
+function requestIsProcessing(){
+  $('#messages').html("<p>Processing your request</p>");
+  $('input').prop('disabled', true);
+}
+
+function requestIsDone(){
+  $('#messages').html("<p>Request Successful!</p>");
+  $('input').prop('disabled', false);
 }
 
 $( document ).ajaxStart(function() {
-  toggleSpinner();
+  requestIsProcessing();
+  $('#spinner').toggle();
 });
 
 $( document ).ajaxStop(function() {
-  toggleSpinner();
+  requestIsDone();
+  $('#spinner').toggle();
 });
 
 
 $(document).ready(function() {
   $('#get_tweets').on('submit', function(e){
     e.preventDefault();
-
     var params = $(this).serialize();
+
     $.ajax({
       type: "GET",
       url:"/",
       data: params
     }).done(function(server_response){
       $('#recent_tweets').html(server_response);
-    }).fail(function(){console.log('wah wah');
+    }).fail(function(){console.log('get tweets fail');
+    });
+
   });
-});
+
+  $('#send_tweet').on('submit', function(e){
+    e.preventDefault();
+    var params = $(this).serialize();
+
+    $.ajax({
+      type: this.method,
+      url: this.action,
+      data: params
+    }).done(function(){
+      requestIsDone();
+      $('#send_tweet input[type=text]').val('');
+    }).fail(function(){console.log('send tweet fail');
+    });
+
+  });
+
 });
